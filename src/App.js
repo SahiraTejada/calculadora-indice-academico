ea9f63f3d8b0844131b8d935a84b265c6c570e71
SahiraTejada <Sahira2208@hotmail.com>
2022-06-29 12:19:53 -0400

    init

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Modal,Table,Row,Col,Button,Alert,Breadcrumb,Card,Form} from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>Calculadora de Indice Academico</h1>
+      <Button>
+        Prueba
+      </Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
