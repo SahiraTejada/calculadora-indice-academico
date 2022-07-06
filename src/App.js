7ee2b7ba4a54be62817a1e5d5a45a4eb617aed01
SahiraTejada <Sahira2208@hotmail.com>
2022-07-05 20:50:36 -0400

    Example 2

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Modal,Table,Row,Col,Button,Alert,Breadcrumb,Card,Form} from 'react-bootstrap';
 import Login from './components/login/Login';
+
 //Archivo tipo javascript que crea el componente pricipal que va contener toda la estructura de la aplicacion
 
 function App() {
---
+++ src/components/questions/questions.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const questions = () => {
+  return (
+    <>
+    </>
+  )
+}
+
+export default questions
